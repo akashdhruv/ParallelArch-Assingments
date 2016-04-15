b349c5d3040c35925a36c8aeb23dd17617986501
akash <akashdhruv@gwmail.gwu.edu>
2016-04-15 15:27:06 -0500

    Adding more calls

--- loop_main.F90
+++ loop_main.F90
@@ -9,10 +9,10 @@ program loop_main
    real :: time
    integer :: threads
 
-   M = 2**13
-   N = 2**13
+   M = 2**12
+   N = 2**12
 
-   threads = 2**1
+   threads = 2**2
 
    ! Function call loop_naive(rows(in), columns(in), threads(in), time(out) )
    call loop_naive(M,N,threads,time)
@@ -28,11 +28,31 @@ program loop_main
 
    print '("Tiled Loop Time = ",f10.7," seconds.")',time
 
-   i_tile = 2**3
-   j_tile = 2**3
+   i_tile = 2**4
+   j_tile = 2**4
 
    ! Function call loop_tile(rows(in), columns(in), threads(in), i_tile(in), j_tile (in) time(out))
    call loop_tile(M,N,threads,i_tile,j_tile,time)
+
+
+   print '("Tiled Loop Time = ",f10.7," seconds.")',time
+
+   i_tile = 2**8
+   j_tile = 2**8
+
+   ! Function call loop_tile(rows(in), columns(in), threads(in), i_tile(in),
+   ! j_tile (in) time(out))
+   call loop_tile(M,N,threads,i_tile,j_tile,time)
+
+
+   print '("Tiled Loop Time = ",f10.7," seconds.")',time
+
+   i_tile = 2**10
+   j_tile = 2**10
+
+   ! Function call loop_tile(rows(in), columns(in), threads(in), i_tile(in),
+   ! j_tile (in) time(out))
+   !call loop_tile(M,N,threads,i_tile,j_tile,time)
 
 
    print '("Tiled Loop Time = ",f10.7," seconds.")',time
